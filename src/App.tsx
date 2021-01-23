bdc33a42624be7a53a066710e9e00965f8166a42
Theo Iyonor <symplytheo@gmail.com>
2021-01-23 20:17:00 +0100

    Import and render all sections with props type

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import Navigation from 'sections/Navigation'
+import Header from 'sections/Header'
+import Features from 'sections/Features'
+import About from 'sections/About'
+import Services from 'sections/Services'
+import Gallery from 'sections/Gallery'
+import Testimonials from 'sections/Testimonials'
+import Team from 'sections/Team'
+import Contact from 'sections/Contact'
+import { 
+  AboutProps,
+  ContactProps,
+  FeatureProps,
+  HeaderProps,
+  ServiceProps,
+  TeamProps,
+  TestimonialProps
+} from 'types'
+import JsonData from './data/data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type PageDataType = {
+  Header: HeaderProps
+  About: AboutProps,
+  Contact: ContactProps,
+  Features: FeatureProps[],
+  Services: ServiceProps[],
+  Team: TeamProps[],
+  Testimonials: TestimonialProps[]
+}
+export class App extends Component {
+  state = {
+    data: {} as PageDataType,
+  }
+
+  getData() {
+    this.setState({data : JsonData})
+  }
+
+  componentWillMount() {
+    this.getData()
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div>
+        <Navigation />
+        <Header data={data.Header} />
+        <Features data={data.Features} />
+        <About data={data.About} />
+        <Services data={data.Services} />
+        <Gallery />
+        <Testimonials data={data.Testimonials} />
+        <Team data={data.Team} />
+        <Contact data={data.Contact} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
